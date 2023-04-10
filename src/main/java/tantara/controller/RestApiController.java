94f9abf9ab5fb79649e0d4c03b7a1447e0ee9890
dlaej <dladmswj0822@naver.com>
2023-04-10 12:03:40 +0900

    test

--- src/main/java/tantara/controller/RestApiController.java
+++ src/main/java/tantara/controller/RestApiController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RestApiController {
-	int i = 0;
+	
 }
